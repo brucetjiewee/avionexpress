e024c8e1744709feab3b6a46d2b4cdbbc98552be
Bruce Liu <bruceliu@Bruces-MacBook-Air.local>
2018-11-16 18:50:56 +0200

    privacy policy

--- Email.php
+++ Email.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 if(isset($_POST['submit'])){
 $toinfo = "cephascollins@gmail.com";
 $from = $_POST['email'];
@@ -6,13 +6,10 @@ $name = $_POST['name'];
 $tel = $_POST['tel'];
 $subject = $_POST['subject'];
 $comment = $_POST['comment'];
-$message = "Name: ".$name.
-"\n"."Contact number: ".$tel.
-"\n"."Comments:".$comment;
+$message = "Name: ".$name."\n"."Contact number: ".$tel."\n"."Comments:".$comment;
 $headers = "From:".$from;
 mail($toinfo,$subject,$message,$headers);
 echo "<script>alert('Mail Sent. Thank you ". $name . ", we will contact you shortly.')</script>";
 echo "<meta http-equiv=\"refresh\" content=\"0; url=index.html\" />";
 }
-
 ?>
