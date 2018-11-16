db9505b3711b9ff3785aefaf1861df49a57a5115
Bruce Liu <bruceliu@Bruces-MacBook-Air.local>
2018-11-16 18:07:44 +0200

    shit ton of changes

---
+++ Email.php
@@ -0,0 +1,18 @@
+<?php 
+if(isset($_POST['submit'])){
+$toinfo = "cephascollins@gmail.com";
+$from = $_POST['email'];
+$name = $_POST['name'];
+$tel = $_POST['tel'];
+$subject = $_POST['subject'];
+$comment = $_POST['comment'];
+$message = "Name: ".$name.
+"\n"."Contact number: ".$tel.
+"\n"."Comments:".$comment;
+$headers = "From:".$from;
+mail($toinfo,$subject,$message,$headers);
+echo "<script>alert('Mail Sent. Thank you ". $name . ", we will contact you shortly.')</script>";
+echo "<meta http-equiv=\"refresh\" content=\"0; url=index.html\" />";
+}
+
+?>
