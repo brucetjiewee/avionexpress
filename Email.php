b010023a0cb26a5e39a4f0255d6d6ca72509e43a
Bruce Liu <bruceliu@Bruces-MacBook-Air.local>
2018-11-19 10:25:09 +0200

    email attempt

--- Email.php
+++ Email.php
@@ -1,15 +1,15 @@
 <?php
 if(isset($_POST['submit'])){
-$toinfo = "cephascollins@gmail.com";
+$toinfo = "brucetjie.wee@gmail.com";
 $from = $_POST['email'];
 $name = $_POST['name'];
 $tel = $_POST['tel'];
 $subject = $_POST['subject'];
 $comment = $_POST['comment'];
 $message = "Name: ".$name."\n"."Contact number: ".$tel."\n"."Comments:".$comment;
-$headers = "From:".$from;
+$headers = "From: ".$from;
 mail($toinfo,$subject,$message,$headers);
-echo "<script>alert('Mail Sent. Thank you ". $name . ", we will contact you shortly.')</script>";
+echo "<script>alert('Mail Sent. Thank you ".$name.", we will contact you shortly.')</script>";
 echo "<meta http-equiv=\"refresh\" content=\"0; url=index.html\" />";
 }
 ?>
