752c845153a671150ea0e7a1bd65218cc44f52ca
Bruce Liu <bruceliu@Bruces-MacBook-Air.local>
2018-11-20 23:49:17 +0200

    email fix

--- Email.php
+++ Email.php
@@ -1,15 +1,17 @@
 <?php
 if(isset($_POST['submit'])){
-$toinfo = "brucetjie.wee@gmail.com";
+$toinfo = "cephascollins@gmail.com";
+
 $from = $_POST['email'];
 $name = $_POST['name'];
 $tel = $_POST['tel'];
 $subject = $_POST['subject'];
 $comment = $_POST['comment'];
 $message = "Name: ".$name."\n"."Contact number: ".$tel."\n"."Comments:".$comment;
-$headers = "From: ".$from;
+$headers = "From: info@avion-express.co.za"."\r\n";
+$headers .= 'Cc: '.$from . "\r\n";
 mail($toinfo,$subject,$message,$headers);
 echo "<script>alert('Mail Sent. Thank you ".$name.", we will contact you shortly.')</script>";
-echo "<meta http-equiv=\"refresh\" content=\"0; url=index.html\" />";
+echo "<meta http-equiv=\"refresh\" content=\"0; url=contact-us.html\" />";
 }
 ?>
